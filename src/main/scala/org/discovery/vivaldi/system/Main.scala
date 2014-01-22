124964731cd91a0e000b2e5fcad88ef9b3cb1c99
Nicolas Joseph <nikel092@gmail.com>
2014-01-22 13:19:24 +0100

    revert previous commit for vompilation

--- src/main/scala/org/discovery/vivaldi/system/Main.scala
+++ src/main/scala/org/discovery/vivaldi/system/Main.scala
@@ -99,6 +99,7 @@ class Main(name : String,id:Int) extends Actor {
     val rps = rpsIterable.toSeq
 
     coordinates = newCoordinates
+    updateMonitoring
 
     //Computing the distances from the RPS table
     val RPSCloseNodes = rps.map(node => CloseNodeInfo(node.node, node.coordinates,computeDistanceToSelf(node.coordinates)))
@@ -120,6 +121,7 @@ class Main(name : String,id:Int) extends Actor {
     closeNodes = closeNodes.sorted.take(numberOfCloseNodes)
   }
 
+  def updateMonitoring = {}
   /**
    * Method to compute the distance between the current node and the node in parameter
    * @param externCoordinates to compute the distance from
